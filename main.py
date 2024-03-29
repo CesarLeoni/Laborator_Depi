61154ed76d3816e34ed5197397a9a0750dc8f612
CesarLeoni <leoni.cesar.emanuele@gmail.com>
2024-03-29 17:37:02 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# data = np.load("penguins.npy")
+#
+# print(data)
+# x = data[:, 0]
+# y = data[:, 1]
+# plt.scatter(x, y)
+#
+# R=np.mean(x*y)
+# print(R)
+#
+# K=R-np.mean(x)*np.mean(y)
+# print(K)
+#
+# ro=K/(np.std(x)*np.std(y))
+# print(ro)
+#
+# plt.show()
+
+from mpl_toolkits.mplot3d import Axes3D
+pas=0.01
+X = np.arange(4,7,pas)
+Y = np.arange(1,4,pas)
+X, Y = np.meshgrid(X, Y)
+miu = np.array([5.5,2.5])
+sigma = np.array([[1,0.5],[0.5,2]])
+det = np.linalg.det(miu)
+inv =
+f = np.empty((len(X), len(Y)))
+for i in range(len(X)):
+for j in range(len(Y)):
+C = np.array([X[i, j], Y[i, j]])
+f[i, j] = … # 3.14 (X = C)
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+ax.plot_surface(X, Y, f)
+plt.show()
